c00f004e9fcecfa38ab30241bf2b4bacedd1e016
609554070zjf <609554070@qq.com>
2017-09-20 23:46:28 +0800

    上传Struts的Action组件（GoodsAction、GoodsTypeAction、OrderAction、UserAction)

---
+++ ElectronicEmporium-SSH-0815/src/edu/fjnu/cse/action/OrderAction.java
@@ -0,0 +1,128 @@
+/**
+ * 
+ */
+package edu.fjnu.cse.action;
+
+import java.util.HashSet;
+import java.util.Map;
+
+import org.apache.struts2.interceptor.SessionAware;
+
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
+
+import edu.fjnu.cse.domain.BuyCar;
+import edu.fjnu.cse.domain.Goods;
+import edu.fjnu.cse.domain.Order;
+import edu.fjnu.cse.domain.OrderQueryHelper;
+import edu.fjnu.cse.domain.PageBean;
+import edu.fjnu.cse.domain.User;
+import edu.fjnu.cse.service.OrderService;
+import edu.fjnu.cse.service.impl.UserServiceImpl;
+
+/**
+ * @author Javon
+ * 
+ */
+public class OrderAction extends ActionSupport implements ModelDriven<Order>,
+		SessionAware {
+
+	private Order order = new Order();
+	private OrderService orderService;
+	private Map<String, Object> session = null;
+	private OrderQueryHelper helper = new OrderQueryHelper();
+	private PageBean<Order> pb = new PageBean<Order>();
+	private int pageCode = 1;
+
+	@Override
+	public void setSession(Map<String, Object> session) {
+		this.session = session;
+	}
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+
+	public OrderService getOrderService() {
+		return orderService;
+	}
+
+	public void setOrderService(OrderService orderService) {
+		this.orderService = orderService;
+	}
+	
+	public OrderQueryHelper getHelper() {
+		return helper;
+	}
+
+	public void setHelper(OrderQueryHelper helper) {
+		this.helper = helper;
+	}
+
+	public int getPageCode() {
+		return pageCode;
+	}
+
+	public void setPageCode(int pageCode) {
+		this.pageCode = pageCode;
+	}
+	
+	public PageBean<Order> getPb() {
+		return pb;
+	}
+
+	public void setPb(PageBean<Order> pb) {
+		this.pb = pb;
+	}
+
+	@Override
+	public Order getModel() {
+		return order;
+	}
+
+	public String loadOrderByUserId() throws Exception{
+		User user = (User) session.get("user");
+		helper.setUser(user);
+		pb = orderService.loadAllByPage(helper, pb);
+		return "load_order_page_all_user";
+	}
+	
+	public String getOrderByOrderId() throws Exception{
+		User user = (User) session.get("user");
+		helper.setUser(user);
+		order  = orderService.loadOrderById(order.getOrderId());
+		return "load_order_page_user";
+	}
+	
+	public String getOrderByOrderIdAdmin() throws Exception{
+		User user = (User) session.get("user");
+		helper.setUser(user);
+		order  = orderService.loadOrderById(order.getOrderId());
+		return "load_order_page_user_admin";
+	}
+
+	public String loadAll() throws Exception {
+		helper.setUser(null);
+		pb = orderService.loadAllByPage(helper, pb);
+		return "load_order_page_all";
+	}
+
+	public String createOrder() throws Exception {
+		System.out.println(order);
+		User user = (User) session.get("user");
+		order.setUser(user);
+		HashSet<BuyCar> buyCarSet = (HashSet<BuyCar>) session.get("buycar");
+		order.getBuycarList().addAll(buyCarSet);
+		for (BuyCar buycar : buyCarSet) {
+			buycar.setOrder(order);
+		}
+		orderService.saveOrder(order);
+		session.remove("buycar");
+		return "order_success";
+	}
+
+}
---
+++ ElectronicEmporium-SSH-0815/src/edu/fjnu/cse/action/UserAction.java
@@ -0,0 +1,202 @@
+/**
+ * 
+ */
+package edu.fjnu.cse.action;
+
+import java.util.Map;
+
+import org.apache.struts2.interceptor.SessionAware;
+
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
+
+import edu.fjnu.cse.domain.GoodsQueryHelper;
+import edu.fjnu.cse.domain.PageBean;
+import edu.fjnu.cse.domain.User;
+import edu.fjnu.cse.domain.UserQueryHelper;
+import edu.fjnu.cse.service.UserService;
+
+/**
+ * @author Javon
+ *
+ */
+public class UserAction extends ActionSupport implements ModelDriven<User>,SessionAware{
+	private UserService userService;
+	private UserQueryHelper helper = new UserQueryHelper();
+	private User user = new User();
+	private PageBean<User> pb = new PageBean<User>();
+	private int pageCode = 1;
+	private Map<String, Object> session = null;
+	private String pwd;
+	private String newPwd;
+
+
+	@Override
+	public User getModel() {
+		return user;
+	}
+	public UserService getUserService() {
+		return userService;
+	}
+	public void setUserService(UserService userService) {
+		this.userService = userService;
+	}
+	public UserQueryHelper getHelper() {
+		return helper;
+	}
+	public void setHelper(UserQueryHelper helper) {
+		this.helper = helper;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public PageBean<User> getPb() {
+		return pb;
+	}
+	public void setPb(PageBean<User> pb) {
+		this.pb = pb;
+	}
+	public int getPageCode() {
+		return pageCode;
+	}
+	public void setPageCode(int pageCode) {
+		this.pageCode = pageCode;
+	}
+	
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getNewPwd() {
+		return newPwd;
+	}
+	public void setNewPwd(String newPwd) {
+		this.newPwd = newPwd;
+	}
+	public String toAdd() throws Exception{
+		return "add_page";
+	}
+	
+	public String add() throws Exception{
+		System.out.println(user);
+		userService.saveUser(user);
+		if(session.get("user") != null){
+			return "loadAllAdmin";
+		}
+		else return "login_page";
+	}
+	
+	public String toUpdate() throws Exception{
+		user = userService.getUserById(user.getUserId());
+		return "update_page";
+	}
+	
+	public String update() throws Exception{
+		user.setPassword(userService.getUserById(user.getUserId()).getPassword());
+		userService.updateUser(user);
+		return "loadAllAdmin";
+	}
+	
+	public String delete() throws Exception{
+		userService.deleteUser(user.getUserId());
+		return "loadAllAdmin";
+	}
+	
+	public String loadAllAdmin() throws Exception{
+		pb.setPageSize(10);
+		System.out.println(helper);
+		pb = userService.loadAllAdminByPage(helper,pb);
+		return "load_page";
+	}
+	
+	public String infoAdmin() throws Exception{
+		user = userService.getUserById(user.getUserId());
+		return "info_page";
+	}
+	
+	public String login() throws Exception{
+		user = userService.findUserByUserNameAndPWD(user);
+		session.clear();
+		if(user != null){
+			session.put("user", user);
+		}
+		else{
+			this.addActionError("用户名或密码或身份错误！");
+			return "login_page";
+		}
+		if(user.getIdentify() == 2){
+			return "toUserMain";
+		}
+		else return "loginSuccess_admin";
+	}
+	@Override
+	public void setSession(Map<String, Object> session) {
+		this.session = session;
+	}
+	
+	public String infoUser() throws Exception{
+		int userId = ((User)session.get("user")).getUserId();
+		user = userService.getUserById(userId);
+		return "info_page_user";
+	}
+	
+	public String toUpdateUser() throws Exception{
+		int userId = ((User)session.get("user")).getUserId();
+		user = userService.getUserById(userId);
+		return "update_page_user";
+	}
+	
+	public String toUserMain() throws Exception{
+		return "loginSuccess_user";
+	}
+	
+	public String updateUser() throws Exception{
+		user.setPassword(userService.getUserById(user.getUserId()).getPassword());
+		userService.updateUser(user);
+		return "toUserMain";
+	}
+	
+	public String toUpdatePwd() throws Exception{
+		return "update_pwd";
+	}
+	
+	public String toReg() throws Exception{
+		return "reg_page";
+	}
+	
+	public String loadAllUser() throws Exception{
+		pb.setPageSize(10);
+		System.out.println(helper);
+		pb = userService.loadAllUserByPage(helper,pb);
+		return "load_page";
+	}
+	
+	public String updatePassword() throws Exception{
+		int userId = ((User)session.get("user")).getUserId();
+		int identify = ((User)session.get("user")).getIdentify();
+		user.setPassword(pwd);
+		user = userService.findUserByUserNameAndPWD(user);
+		if(user != null){
+			user.setPassword(newPwd);
+			userService.updatePassword(user);
+		}
+		else{
+			this.addActionError("原密码错误！");
+			return "update_pwd";
+		}
+		if(user.getIdentify() == 2){
+			return "toUserMain";
+		}
+		else return "loginSuccess_admin";
+	}
+	
+	public String quitSys() throws Exception{
+		session.clear();
+		return "login_page";
+	}
+}
